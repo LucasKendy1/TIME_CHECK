f49abe0fb87ba13950e2d1b39e056b6ec074d292
LucasKendy1 <87815996+LucasKendy1@users.noreply.github.com>
2023-04-14 14:45:27 -0300

    historico pronto

--- scripthome.js
+++ scripthome.js
@@ -1,6 +1,20 @@
 import nome from "./script.js"
 
 var nomeDisplay = document.getElementById('idnome')
+var containerSlide = document.getElementById('idslide')
+var containerH1 = document.getElementById('idh1')
+
+containerSlide.addEventListener('click',abrirHistorico)
 
 nomeDisplay.innerHTML=nome
+
+function abrirHistorico(){
+    if(containerH1.style.height=='90vh'){
+        containerH1.style.height='65px'
+    }
+    else{
+        containerH1.style.height='90vh'
+    }
+}
+
 
