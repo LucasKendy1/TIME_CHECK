aa8340494addd759f020c75d10d29edf62db635c
LucasKendy1 <87815996+LucasKendy1@users.noreply.github.com>
2023-04-15 11:44:15 -0300

    testar no cllr

--- scripthome.js
+++ scripthome.js
@@ -6,10 +6,14 @@ var containerH1 = document.getElementById('idh1')
 var containerHistorico = document.getElementById('idhistory')
 var containerMenu = document.getElementById('idmenu')
 var containerMain = document.getElementById('idmain')
+var header = document.querySelector('header')
+var main = document.querySelector('main')
 
 containerSlide.addEventListener('click',abrirHistorico)
 containerHistorico.addEventListener('click',abrirHistorico)
 containerMenu.addEventListener('click',abrirMenu)
+header.addEventListener('click',abrirHeader)
+main.addEventListener('click', resetAbas)
 
 nomeDisplay.innerHTML=nome
 
@@ -32,4 +36,28 @@ function abrirMenu(){
     containerAba.style.height='100%'
     containerAba.style.backgroundColor='white'
     containerAba.innerHTML=`aaaa`
+}
+
+function abrirHeader(){
+    header.style.transition='1s'
+    if(header.style.height=='90vh'){
+        header.style.height='20vh'
+        header.style.backgroundColor='#f25044'
+        containerH1.style.display='block'
+        containerSlide.style.display='block'
+        main.style.display='block'
+    }
+    else{
+        header.style.height='90vh'
+        header.style.backgroundColor='#233c4d'
+        containerH1.style.display='none'
+        containerSlide.style.display='none'
+        main.style.display='none'
+    }
+}
+
+function resetAbas(){
+    containerH1.style.height='65px'
+    containerSlide.style.transform='rotate(0deg)'
+    header.style.height='20vh'
 }
